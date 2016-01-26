0138d876c69a24fed1e6032218c672f72a02b69f
nperez <nelsonrp24@gmail.com>
2016-01-26 15:39:04 -0300

    Add support for Centos 6.4

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,4 +3,13 @@ package "telnet"
 package "nmap"
 
 
+puts node['platform']
+
+if node['platform'] == "ubuntu"
+	package "apache2"
+elsif node['platform'] == "centos"
+	package "httpd"
+end
+
+
 log "Well, that was too easy"
